29932d956591f6e3d43d80783734a92636cc23cf
Daniil Alekseev <55713843+Dapucla@users.noreply.github.com>
2024-09-04 08:58:44 +0300

    TD-12: main_screen.dart: app bar color fix, real messaging enables, autoscrolling added

--- lib/ui/main_screen.dart
+++ lib/ui/main_screen.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'dart:io';
+import 'dart:math';
 
 import 'package:audioplayers/audioplayers.dart';
 import 'package:dart_openai/dart_openai.dart';
@@ -10,52 +11,15 @@ import 'package:flutter_sound/flutter_sound.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:logger/logger.dart';
-import 'package:tark_openai_whisper_poc/util/color_extension.dart';
+import 'package:record/record.dart';
+import 'package:flutter/scheduler.dart' show TickerProvider, TickerProviderStateMixin;
+
 import 'package:tark_openai_whisper_poc/util/context_extensions.dart';
 
 import '../util/log.dart';
 import 'common_widgets/texts.dart';
 import 'ui_constants.dart';
 
-final mockMessages = [
-  Message(
-    audioFilePath: 'test',
-  ),
-  Message(
-    whisper: Whisper(
-      transcription: 'Может ли иос симулятор использовать микрофон компьютера?',
-      translation: 'Can the iOS simulator use the computer\'s microphone?',
-    ),
-  ),
-  Message(
-    audioFilePath: 'test',
-  ),
-  Message(
-    whisper: Whisper(
-      language: 'english',
-      transcription: 'Can the iOS simulator use the computer\'s microphone?',
-      translation: 'Can the iOS simulator use the computer\'s microphone?',
-    ),
-  ),
-  Message(
-    audioFilePath: 'test',
-  ),
-  Message(
-    whisper: Whisper(
-      transcription: 'Может ли иос симулятор использовать микрофон компьютера?',
-      translation: 'Can the iOS simulator use the computer\'s microphone?',
-    ),
-  ),
-  Message(
-    audioFilePath: 'test',
-  ),
-  Message(
-    whisper: Whisper(
-      transcription: 'Может ли иос симулятор использовать микрофон компьютера?',
-      translation: 'Can the iOS simulator use the computer\'s microphone?',
-    ),
-  ),
-];
 
 class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
@@ -64,22 +28,35 @@ class MainScreen extends StatefulWidget {
   _MainScreenState createState() => _MainScreenState();
 }
 
-class _MainScreenState extends State<MainScreen> {
-  FlutterSoundRecorder? _recorder;
-  bool _isRecording = false;
-  // String? _filePath;
-  StreamSubscription? _recorderSubscription;
-  double _decibels = 0.0;
+class _MainScreenState extends State<MainScreen> with TickerProviderStateMixin {
+  final ScrollController _scrollController = ScrollController();
   final _player = AudioPlayer();
-  var _messages = mockMessages;
+
+  late final AudioRecorder _recorder;
+  bool _isRecording = false;
+  String? _filePath;
+  StreamSubscription? _recorderSubscription;
+  List<Message> _messages = [];
+
+  late AnimationController _animationController;
+  late Animation<double> _animation;
 
   @override
   void initState() {
     super.initState();
-    _recorder = FlutterSoundRecorder(
-      logLevel: Level.nothing,
-    );
+    _recorder = AudioRecorder();
     _initializeRecorder();
+
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 500),
+    );
+
+    _animation = Tween<double>(begin: 1.0, end: 1.5).animate(
+        CurvedAnimation(parent: _animationController, curve: Curves.easeInOut))
+      ..addListener(() {
+        setState(() {});
+      });
   }
 
   @override
@@ -100,6 +77,7 @@ class _MainScreenState extends State<MainScreen> {
         child: Stack(
           children: [
             ListView.separated(
+              controller: _scrollController,
               itemCount: _messages.length,
               padding: AppPadding.bottom(160),
               itemBuilder: (c, i) => _messageItem(_messages[i]),
@@ -112,17 +90,15 @@ class _MainScreenState extends State<MainScreen> {
               left: 0,
               right: 0,
               child: SizedBox(
-              height: 300,
-              width: 300,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  _micButton(),
-
-                ],
-              ),
-            ),),
-
+                height: 300,
+                width: 300,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    _micButton(),
+                  ],
+                ),
+              ),),
           ],
         ),
       ),
@@ -131,89 +107,140 @@ class _MainScreenState extends State<MainScreen> {
 
   @override
   void dispose() {
-    _recorder?.closeRecorder();
+    if (_isRecording) {
+      _stopRecording();
+    }
     _recorderSubscription?.cancel();
+    _scrollController.dispose();
     super.dispose();
   }
 
-  //
   Future<void> _initializeRecorder() async {
     final status = await Permission.microphone.request();
     if (status != PermissionStatus.granted) {
       throw RecordingPermissionException('Microphone permission not granted');
     }
+  }
 
-    await _recorder?.openRecorder();
+  String _generateRandomId() {
+    const chars = 'abcdefghijklmnopqrstuvwxyz0123456789';
+    final random = Random();
+    return List.generate(
+      10,
+          (index) => chars[random.nextInt(chars.length)],
+      growable: false,
+    ).join();
   }
 
   Future<void> _startRecording() async {
-    l('_startRecording');
-
-    final directory = await getTemporaryDirectory();
-    // _filePath =
-    //     '${directory.path}/audio_${DateTime.now().millisecondsSinceEpoch}.wav';
-
-    await _recorder?.setSubscriptionDuration(
-      const Duration(milliseconds: 100),
-    );
-
-    _recorderSubscription = _recorder?.onProgress?.listen((e) {
-      l('_startRecording', 'decibels: ${e.decibels}');
-      setState(() {
-        _decibels = e.decibels ?? 0.0;
-      });
-    });
-
-    try {
-      await _recorder?.startRecorder(
-        // toFile: _filePath,
-      );
-    } catch (e) {
-      l('_startRecording', 'error: $e');
-    }
-
     setState(() {
       _isRecording = true;
     });
+
+    try {
+      l('=========>>>>>>>>>>> RECORDING!!!!!!!!!!!!!!! <<<<<<===========');
+
+      String filePath = await getApplicationDocumentsDirectory()
+          .then((value) => '${value.path}/${_generateRandomId()}.wav');
+
+      await _recorder.start(
+        const RecordConfig(
+          encoder: AudioEncoder.wav,
+        ),
+        path: filePath,
+      );
+
+      List<double> decibelReadings = [];
+
+      _recorderSubscription = _recorder.onAmplitudeChanged(const Duration(milliseconds: 300)).listen((amp) {
+        double absoluteAmplitude = amp.current.abs();
+
+        l('Amplitude current (absolute):', absoluteAmplitude.toString());
+
+        if (absoluteAmplitude > 0) {
+          double decibels = 20 * log(absoluteAmplitude + 1) / log(10);
+
+          if (decibelReadings.length >= 10) {
+            decibelReadings.removeAt(0);
+          }
+          decibelReadings.add(decibels);
+        } else {
+          l('Decibels:', 'Amplitude is too low to calculate decibels.');
+        }
+      });
+
+      Timer.periodic(const Duration(seconds: 5), (Timer timer) {
+        if (decibelReadings.isNotEmpty) {
+          double averageDecibels = decibelReadings.reduce((a, b) => a + b) / decibelReadings.length;
+          l('Average Decibels over last 5 seconds:', averageDecibels.toString());
+
+          if (averageDecibels < 60.0) {
+            timer.cancel();
+            setState(() {
+              _isRecording = false;
+            });
+          }
+        }
+      });
+    } catch (e) {
+      l('_startRecording', 'error: $e');
+    }
   }
 
   Future<void> _stopRecording() async {
-    await _recorder?.stopRecorder();
     _recorderSubscription?.cancel();
 
     setState(() {
       _isRecording = false;
     });
-    //
-    // OpenAI.apiKey = dotenv.env['OPENAI_API_KEY'] ?? '';
-    // OpenAIAudioModel transcription =
-    //     await OpenAI.instance.audio.createTranscription(
-    //   file: File(_filePath ?? ''),
-    //   model: "whisper-1",
-    //   responseFormat: OpenAIAudioResponseFormat.json,
-    // );
-    //
-    // l('build', 'duration:   ${transcription.duration}');
-    // l('build', 'language:   ${transcription.language}');
-    // l('build', 'text:       ${transcription.text}');
 
-    // _whispers.add(
-    //   WhisperObject(
-    //     transcription: transcription.text,
-    //     translation: 'translation',
-    //   language: transcription.language,
-    //   ),);
+    String? path = await _recorder.stop();
+    if (path == null) return;
 
-    // setState(() {
-    //   _whispers = [
-    //     ..._whispers,
-    //     Whisper(
-    //       transcription: transcription.text,
-    //       translation: 'translation',
-    //       language: transcription.language,
-    //     ),
-    //   ];
-    // });
+    setState(() {
+      _filePath = path;
+    });
+
+    debugPrint('=========>>>>>> PATH: $_filePath <<<<<<===========');
+
+    OpenAI.apiKey = dotenv.env['OPENAI_API_KEY'] ?? '';
+    OpenAIAudioModel transcription =
+    await OpenAI.instance.audio.createTranscription(
+      file: File(_filePath ?? ''),
+      model: "whisper-1",
+      responseFormat: OpenAIAudioResponseFormat.json,
+    );
+
+    OpenAIAudioModel tranlsation = await OpenAI.instance.audio.createTranslation(
+      file: File(_filePath!),
+      model: "whisper-1",
+      responseFormat: OpenAIAudioResponseFormat.json,
+    );
+
+    l('build', 'duration:   ${transcription.duration}');
+    l('build', 'language:   ${transcription.language}');
+    l('build', 'text:       ${transcription.text}');
+    l('build', 'translation:${tranlsation.text}');
+
+    setState(() {
+      _messages.add(Message(
+        whisper: Whisper(
+          transcription: transcription.text,
+          translation: tranlsation.text,
+        ),
+      ));
+      _scrollToBottom();
+    });
+  }
+
+  void _scrollToBottom() {
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      _scrollController.animateTo(
+        _scrollController.position.maxScrollExtent,
+        duration: const Duration(milliseconds: 300),
+        curve: Curves.easeOut,
+      );
+    });
   }
 
   Widget _messageItem(Message message) {
@@ -221,7 +248,7 @@ class _MainScreenState extends State<MainScreen> {
     final whisper = message.whisper;
     return Row(
       mainAxisAlignment:
-          filePath == null ? MainAxisAlignment.start : MainAxisAlignment.end,
+      filePath == null ? MainAxisAlignment.start : MainAxisAlignment.end,
       children: [
         if (filePath != null) _audioFilePath(filePath),
         if (whisper != null) Expanded(
@@ -250,17 +277,17 @@ class _MainScreenState extends State<MainScreen> {
             maxLines: 100,
           ),
           if (whisper.language != 'english') ...[
-          Divider(
-            color: context.primary.withOpacity(0.25),
-            height: 30,
-            thickness: 0.5,
-          ),
-          Texts(
-            whisper.translation,
-            fontSize: AppSize.fontNormal,
-            maxLines: 100,
-            color: context.primary.withOpacity(0.4),
-          ),],
+            Divider(
+              color: context.primary.withOpacity(0.25),
+              height: 30,
+              thickness: 0.5,
+            ),
+            Texts(
+              whisper.translation,
+              fontSize: AppSize.fontNormal,
+              maxLines: 100,
+              color: context.primary.withOpacity(0.4),
+            ),],
         ],
       ),
     );
@@ -338,13 +365,12 @@ class _MainScreenState extends State<MainScreen> {
   }
 
   Widget _micButton() {
-
     return Container(
-      width: 300,
-      height: 300,
-      padding: AppPadding.all(100),
+      width: 300 * _animation.value,
+      height: 300 * _animation.value,
+      padding: AppPadding.all(100 * _animation.value),
       decoration: BoxDecoration(
-        shape: BoxShape.circle, // This makes the container circular
+        shape: BoxShape.circle,
         gradient: RadialGradient(
           colors: [
             context.background,
@@ -354,9 +380,7 @@ class _MainScreenState extends State<MainScreen> {
         ),
       ),
       child: _button(
-        icon: _isRecording
-            ? Icons.stop_outlined
-            : Icons.mic_none_outlined,
+        icon: _isRecording ? Icons.stop_outlined : Icons.mic_none_outlined,
         onPressed: () async {
           if (_isRecording) {
             await _stopRecording();
@@ -370,14 +394,14 @@ class _MainScreenState extends State<MainScreen> {
 
   Widget _playButton(){
     return Padding(
-        padding: AppPadding.leftNormal,
-        child: _button(
-          icon: Icons.play_arrow_outlined,
-          onPressed: () async {
-            // await _player.play(DeviceFileSource(_filePath ?? ''));
-          },
-        ),
-      );
+      padding: AppPadding.leftNormal,
+      child: _button(
+        icon: Icons.play_arrow_outlined,
+        onPressed: () async {
+          // await _player.play(DeviceFileSource(_filePath ?? ''));
+        },
+      ),
+    );
   }
 }
 
@@ -497,7 +521,6 @@ class _AudioMessageState extends State<AudioMessage> {
             samples: [],
             height: 10,
             width: 1,
-
             // color: Colors.blue,
             // playerController: _controller,
             // waveformType: WaveformType.bar,
